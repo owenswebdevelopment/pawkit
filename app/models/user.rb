da54fa83e148f134f74cbefc16b30025e110d17c
Hayao Mori <hayaomori312@gmail.com>
2025-05-27 17:23:20 +0900

    created footer but links are not connected yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_families
   has_many :families, through: :user_families
+  has_one_attached :photo
 end
