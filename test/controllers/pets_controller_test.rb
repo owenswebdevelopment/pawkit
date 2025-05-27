a42e7c308cee8176e6d5219a14559bf72d820fb4
Hayao Mori <hayaomori312@gmail.com>
2025-05-27 12:01:10 +0900

    inital pets controller

---
+++ test/controllers/pets_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PetsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get pets_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get pets_create_url
+    assert_response :success
+  end
+end
