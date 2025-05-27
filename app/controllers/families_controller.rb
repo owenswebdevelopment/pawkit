19a020f91d13cdc02078e8441c3a139afc8f2191
owen newsome <melfordmalichai@gmail.com>
2025-05-27 13:59:35 +0900

    chore: placeholder do not push

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,5 +1,35 @@
 class FamiliesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :join]
+
   def index
-    @family = Family.all
+    @families = Family.all
+  end
+
+  def join_family_action
+    @family = Family.find(params[:id])
+    current_user.update(family: @family)
+    redirect_to family_path(@family), notice: "You have joined the family!"
+  end
+
+  def show
+    @family = Family.find(params[:id])
+  end
+
+  def new
+    @family = Family.new
+  end
+
+  def create
+    @family = Family.new(strong_params)
+    @family.user = current_user
+    if @family.save
+      redirect_to family_path(@family), notice: "Successfully created family"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def strong_params
+    params.require(:family).permit(:name)
   end
 end
