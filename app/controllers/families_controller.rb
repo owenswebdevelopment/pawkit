cd2e71e29d97c7642a1095bba9ca1c438778bf2c
owen newsome <melfordmalichai@gmail.com>
2025-05-27 14:34:23 +0900

    WIP: progress on FamiliesController

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,5 +1,5 @@
 class FamiliesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :join]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @families = Family.all
