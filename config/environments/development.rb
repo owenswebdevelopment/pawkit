f4d1b3ea67f799e36e000bc9fede8f056b4086eb
Hayao Mori <hayaomori312@gmail.com>
2025-05-27 18:20:22 +0900

    completed PWA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Allow requests by adding the following to your environment configuration
+  config.hosts.clear
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
