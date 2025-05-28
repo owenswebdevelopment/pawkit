a9413ba113380e6c15a8caaad7971d02fab34f86
Tsering Lama <tseriing.boksha@gmail.com>
2025-05-28 12:58:19 +0900

    modified schema, change routes for update, partially done with update task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_015921) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_074253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -108,6 +108,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_015921) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
     t.index ["pet_id"], name: "index_tasks_on_pet_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
