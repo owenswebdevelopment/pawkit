4719b7e2c544d968f68f9f3d815e21649df68b80
owen newsome <melfordmalichai@gmail.com>
2025-05-28 12:43:12 +0900

    added text to feed

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,11 +1,34 @@
 class MemoriesController < ApplicationController
 
   def index
-   @memories = Memory.all
+   @family = Family.find(params[:family_id])
+   @memory = Memory.new
+   @memories = @family.memories
+  #  @memory = current_user.family
   #  @memories = current_user.memories
   end
 
-# def create
+  def create
+    @family = Family.find(params[:family_id])
+    @memory = Memory.new(message_params)
+    @memory.user = current_user
+    if @memory.save!
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(:memories, partial: "memories/memory",
+            target: "memories",
+            locals: { memory: @memory })
+        end
+        format.html { redirect_to family_memories_path(@family) }
+      end
+    else
+      render "", status: :unprocessable_entity
+    end
+  end
 
-# end
+private
+
+def message_params
+  params.require(:memory).permit(:text, :media, :pet_id)
+end
 end
--- app/models/family.rb
+++ app/models/family.rb
@@ -1,5 +1,6 @@
 class Family < ApplicationRecord
   has_many :pets, dependent: :destroy
+  has_many :memories, through: :pets
   has_many :user_families
   has_many :users, through: :user_families
   validates :name, presence: true
