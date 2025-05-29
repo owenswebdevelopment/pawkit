9ef76cbccee3176fedeedcee059d57931f5b0eeb
Hayao Mori <hayaomori312@gmail.com>
2025-05-29 16:04:58 +0900

    fixed footer and connected links from family index to pet show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+
+      redirect_to families_path, notice: "Successfully created family"
+
+    end
   end
 end
