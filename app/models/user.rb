63cb3c49ac3c8eb4047a0a48e46a3b16cd6f9ac9
Tsering Lama <tseriing.boksha@gmail.com>
2025-06-02 11:33:22 +0900

    added completed by to task

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_families
   has_many :families, through: :user_families
+  has_many :completed_tasks, class_name: 'Task', foreign_key: 'completed_by'
   has_one_attached :photo
 end
