a51ebaab52036518979af96944fe280057639cd8
Hayao Mori <hayaomori312@gmail.com>
2025-06-03 14:16:11 +0900

    testing for heroku

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,6 +17,8 @@ class PetsController < ApplicationController
     @pet.family = @family
 
     if @pet.save
+      send_line_notification("A new pet has been added: #{@pet.name}")
+
       redirect_to pet_path(@pet), notice: 'Pet was successfully created!'
     else
       @family = Family.find(params[:family_id])
@@ -49,5 +51,23 @@ class PetsController < ApplicationController
 
   def pet_params
     params.require(:pet).permit(:name, :age, :gender, :color, :birthdate, :species, :photo)
+  end
+
+  def send_line_notification(message)
+    uri = URI.parse("https://api.line.me/v2/bot/message/push")
+    request = Net::HTTP::Post.new(uri)
+    request["Authorization"] = "Bearer #{ENV.fetch('LINE_CHANNEL_ACCESS_TOKEN')}"
+
+    request["Content-Type"] = "application/json"
+    request.body = {
+      to: ENV["LINE_GROUP_ID"], # Replace with actual group ID
+      messages: [{ type: "text", text: message }]
+    }.to_json
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+   puts response.body
   end
 end
