b11dfc86b94c8968986ff47af41a4aa772cf5544
turibhai <tseriing.boksha@gmail.com>
2025-06-04 17:34:28 +0900

    debugged

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 
 ActiveRecord::Schema[7.1].define(version: 2025_06_03_030217) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
