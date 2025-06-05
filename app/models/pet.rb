a72e41abed7a9660042b30702828aaf76d164c01
owen newsome <melfordmalichai@gmail.com>
2025-06-05 10:05:48 +0900

    changed see

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -16,6 +16,6 @@ class Pet < ApplicationRecord
     Task.create!(title: "Walk #{name}", due_date: Date.today, pet: self, user: self.family.users.first, recurrence: "daily", description: "Don't forget to walk #{name} at the usual time!") if species.downcase == "dog"
     Task.create!(title: "Groom #{name}", due_date: Date.today, pet: self, user: self.family.users.first, recurrence: "daily", description: "Don't forget to groom #{name} at the usual time!")
     Task.create!(title: "Bath #{name}", due_date: Date.today, pet: self, user: self.family.users.first, recurrence: "weekly", description: "Don't forget to bath #{name} at the usual time!")
+    Task.create!(title: "Vet Visit #{name}", due_date: Date.today, pet: self, user: self.family.users.first, recurrence: "monthly", description: "Don't forget to take #{name} to the vet!")
   end
-
 end
